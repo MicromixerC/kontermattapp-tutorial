98a50a9cf7768739f03f0020c89e46ef96aa59f7
MicromixerC <128631941+MicromixerC@users.noreply.github.com>
2026-05-16 14:32:21 +0200

    Add files via upload

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { TutorialStep, MiniGameStep, QuizStep } from '@/types/tutorial';
 import { TutorialDialog } from '@/components/tutorial/TutorialDialog';
-import { TutorialHeader } from '@/components/tutorial/TutorialHeader';
-import { TutorialChapterList } from '@/components/tutorial/TutorialChapterList';
 import { useTutorialData } from '@/hooks/useTutorialData';
 import { useTutorialState } from '@/hooks/useTutorialState';
 import { useTutorialActions } from '@/hooks/useTutorialActions';
@@ -11,7 +9,7 @@ const isMiniGameStep = (s: TutorialStep): s is MiniGameStep => 'miniGame' in s;
 const isQuizStep    = (s: TutorialStep): s is QuizStep    => 'quiz' in s;
 
 const Tutorial = () => {
-  const { selectedLanguage, setSelectedLanguage, chapters } = useTutorialData();
+  const { chapters } = useTutorialData();
   const state = useTutorialState();
   const actions = useTutorialActions({
     chapters,
@@ -29,7 +27,6 @@ const Tutorial = () => {
     resetStepState: state.resetStepState,
   });
 
-  // Fixed quiz submit with access to current answers
   const submitQuiz = () => {
     const chapter = chapters.find(c => c.id === state.activeChapter);
     const stepData = chapter?.steps[state.currentStep];
@@ -43,60 +40,30 @@ const Tutorial = () => {
   };
 
   const currentChapter = chapters.find(c => c.id === state.activeChapter);
-  let currentStepData = currentChapter?.steps[state.currentStep];
-
-  // Inject current scenario variant into mini-game step
-  if (currentStepData && isMiniGameStep(currentStepData) && currentStepData.miniGame.scenarios) {
-    const variantKey = `${state.activeChapter}-${state.currentStep}`;
-    const variantIndex = state.miniGameVariants[variantKey] || 0;
-    const scenario = currentStepData.miniGame.scenarios[variantIndex];
-    if (scenario) {
-      currentStepData = {
-        ...currentStepData,
-        miniGame: {
-          ...currentStepData.miniGame,
-          scenarios: [scenario],
-        },
-      };
-    }
-  }
+  const currentStepData = currentChapter?.steps[state.currentStep];
 
   return (
-    <div className="min-h-screen relative">
-      <div className="absolute inset-0" style={{ background: 'radial-gradient(circle, #3fa871 20%, #134d33 100%)' }} />
-      <div className="relative z-10 p-4 pb-8">
-        <div className="max-w-lg mx-auto">
-          <TutorialHeader
-            selectedLanguage={selectedLanguage}
-            onLanguageChange={setSelectedLanguage}
-          />
-          <TutorialChapterList
-            chapters={chapters}
-            selectedLanguage={selectedLanguage}
-            onOpenChapter={state.openChapter}
-          />
-          <TutorialDialog
-            isOpen={!!state.activeChapter}
-            currentChapter={currentChapter}
-            currentStep={state.currentStep}
-            currentStepData={currentStepData}
-            selectedAnswer={state.selectedAnswer}
-            showResult={state.showResult}
-            quizAnswers={state.quizAnswers}
-            quizComplete={state.quizComplete}
-            quizScore={state.quizScore}
-            onClose={state.resetTutorialState}
-            onNextStep={actions.nextStep}
-            onPrevStep={actions.prevStep}
-            onSetStep={state.setCurrentStep}
-            onMiniGameAnswer={actions.handleMiniGameAnswer}
-            onQuizAnswer={actions.handleQuizAnswer}
-            onQuizSubmit={submitQuiz}
-            onResetMiniGame={actions.handleMiniGameReset}
-          />
-        </div>
-      </div>
-    </div>
+    <TutorialDialog
+      isOpen={true}
+      chapters={chapters}
+      currentChapter={currentChapter}
+      currentStep={state.currentStep}
+      currentStepData={currentStepData}
+      selectedAnswer={state.selectedAnswer}
+      showResult={state.showResult}
+      quizAnswers={state.quizAnswers}
+      quizComplete={state.quizComplete}
+      quizScore={state.quizScore}
+      onClose={state.resetTutorialState}
+      onNextStep={actions.nextStep}
+      onPrevStep={actions.prevStep}
+      onSetStep={step => { state.setCurrentStep(step); state.resetStepState(); }}
+      onOpenChapter={(id, startStep) => state.openChapter(id, startStep)}
+      onMiniGameAnswer={actions.handleMiniGameAnswer}
+      onQuizAnswer={actions.handleQuizAnswer}
+      onQuizSubmit={submitQuiz}
+      onResetMiniGame={actions.handleMiniGameReset}
+    />
   );
 };
 
